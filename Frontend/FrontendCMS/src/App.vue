9f4a30d95e2bf82f241ca7768e711ea4c5c42fa4
Hitori <113356370+Hitori638@users.noreply.github.com>
2024-04-10 09:47:43 +0200

    Updated App.vue

--- Frontend/FrontendCMS/src/App.vue
+++ Frontend/FrontendCMS/src/App.vue
@@ -1,8 +1,17 @@
-
 <template>
-
-</template>
-
-<script>
-
-</script>
+    <div id="app">
+      <HomeView />
+    </div>
+  </template>
+  
+  <script>
+  import HomeView from './views/HomeView.vue';
+  
+  export default {
+    name: 'App',
+    components: {
+      HomeView,
+    },
+  };
+  </script>
+  
