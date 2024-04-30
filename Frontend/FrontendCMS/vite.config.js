d85e81e284137648b4e05cdca35691c8f2c9d17f
Hitori <fekecfilip@gmail.com>
2024-04-30 12:07:54 +0200

    Update vite config

--- Frontend/FrontendCMS/vite.config.js
+++ Frontend/FrontendCMS/vite.config.js
@@ -1,6 +1,8 @@
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
+import dotenv from 'dotenv'
 import vue from '@vitejs/plugin-vue'
+dotenv.config()
 
 // https://vitejs.dev/config/
 export default defineConfig({
