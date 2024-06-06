6c40d9e3c79e209fd99b17a9073f638f27e58796
TomH35 <132070682+TomH35@users.noreply.github.com>
2024-06-06 15:46:05 +0200

    Updated authentication and routes

--- Backend/laravel/app/Http/Controllers/AuthController.php
+++ Backend/laravel/app/Http/Controllers/AuthController.php
@@ -9,9 +9,13 @@ use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth:api', ['except' => ['login', 'AdminRegistration']]);
+    }
+
     public function AdminRegistration(Request $request)
     {
-       
         $validatedData = $request->validate([
             'meno' => 'required|max:255',
             'priezvisko' => 'required|max:255',
@@ -19,37 +23,16 @@ class AuthController extends Controller
             'password' => 'required',
         ]);
 
-  
         $admin = new Admin;
-
-     
         $admin->meno = $validatedData['meno'];
         $admin->priezvisko = $validatedData['priezvisko'];
         $admin->email = $validatedData['email'];
         $admin->password = Hash::make($validatedData['password']);
-
-     
         $admin->save();
 
-   
         return response()->json(['message' => 'Admin registered successfully'], 201);
     }
-   
-    /**
-     * Create a new AuthController instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth:api', ['except' => ['login']]);
-    }
 
-    /**
-     * Get a JWT via given credentials.
-     *
-     * @return \Illuminate\Http\JsonResponse
-     */
     public function login()
     {
         $credentials = request(['email', 'password']);
@@ -63,21 +46,11 @@ class AuthController extends Controller
         return $this->respondWithToken($token, $admin);
     }
 
-    /**
-     * Get the authenticated User.
-     *
-     * @return \Illuminate\Http\JsonResponse
-     */
     public function me()
     {
         return response()->json(auth()->user());
     }
 
-    /**
-     * Log the user out (Invalidate the token).
-     *
-     * @return \Illuminate\Http\JsonResponse
-     */
     public function logout()
     {
         auth()->logout();
@@ -85,30 +58,19 @@ class AuthController extends Controller
         return response()->json(['message' => 'Successfully logged out']);
     }
 
-    /**
-     * Refresh a token.
-     *
-     * @return \Illuminate\Http\JsonResponse
-     */
     public function refresh()
     {
         return $this->respondWithToken(auth()->refresh());
     }
 
-    /**
-     * Get the token array structure.
-     *
-     * @param  string $token
-     *
-     * @return \Illuminate\Http\JsonResponse
-     */
-    protected function respondWithToken($token, $admin)
+    protected function respondWithToken($token, $admin = null)
     {
         return response()->json([
             'access_token' => $token,
             'token_type' => 'bearer',
-            'admin_id' => $admin->id,
+            'admin_id' => $admin ? $admin->id : null,
             'expires_in' => auth()->factory()->getTTL() * 60
         ]);
     }
 }
+
