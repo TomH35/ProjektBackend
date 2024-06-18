c3494e9c52135078c120490401172bc88dec9eaf
mMajersky <majerskymiroslav.to@gmail.com>
2024-06-18 18:27:50 +0200

    kapacita

--- Frontend/FrontendCMS/src/components/StageCreate.vue
+++ Frontend/FrontendCMS/src/components/StageCreate.vue
@@ -2,7 +2,6 @@
   <div class="stage-create">
     <div class="card">
       <div class="card-body">
-
         <form @submit.prevent="createStage" class="form">
           <div class="mb-3">
             <label for="name" class="form-label">Stage Name:</label>
@@ -50,6 +49,11 @@
             </div>
 
             <div class="mb-3">
+              <label :for="'eventCapacity' + index" class="form-label">Capacity:</label>
+              <input :id="'eventCapacity' + index" v-model="event.capacity" type="number" class="form-control" required>
+            </div>
+
+            <div class="mb-3">
               <label :for="'eventImage' + index" class="form-label">Event Image:</label>
               <input :id="'eventImage' + index" type="file" class="form-control" @change="handleImageChange(index, $event)">
               <img :src="events[index].previewImage" v-if="events[index].previewImage" alt="Image preview" class="img-thumbnail mt-2"/>
@@ -58,6 +62,7 @@
 
           <button type="button" @click="addEvent" class="btn btn-primary mt-3">Add Event</button>
           <button type="submit" class="btn btn-success mt-3">Create Stage</button>
+          <button type="button" @click="removeEvent(index)" class="btn btn-danger mt-3">Remove Event</button>
         </form>
       </div>
     </div>
@@ -83,6 +88,7 @@ export default {
           link: '',
           description: '',
           image: null,
+          capacity: 0, // Add capacity field to initial event data
         },
       ],
       speakers: [],
@@ -115,6 +121,7 @@ export default {
         link: '',
         description: '',
         image: null,
+        capacity: 0, // Add capacity field to new event data
       });
     },
     handleImageChange(index, event) {
@@ -168,6 +175,3 @@ export default {
   },
 };
 </script>
-
-
-  
