676c79c1ffdb0e102947fc397967b62d6a31d6a9
TomH35 <132070682+TomH35@users.noreply.github.com>
2024-06-19 11:21:20 +0200

    updated database migrations

--- Backend/laravel/database/migrations/17_2024_06_18_161429_add_capacity_to_events_table.php
+++ Backend/laravel/database/migrations/17_2024_06_18_161429_add_capacity_to_events_table.php
@@ -3,7 +3,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddCapacityToEventsTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -28,5 +28,5 @@ class AddCapacityToEventsTable extends Migration
             $table->dropColumn('capacity');
         });
     }
-}
+};
 
